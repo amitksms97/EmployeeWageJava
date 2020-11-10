df7d251b03c101555d3ba42fef6e237fe6f5b93e
amtksms97 <amtksms97@gmail.com>
2020-11-10 14:51:07 +0530

    Added: "Code to calculate daily wage"

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -1,12 +1,33 @@
 import java.util.*;
 public class EmployeeWage {
 	public static void main(String [] args) {
+		int WagePerHour=20;
+		int FullDayHour=8;
+		int HalfDayHour=4;
+		int wage=0;
 		System.out.println("Welcome to Employee Wage Computation Program");
 		Random rnum = new Random();
 		int random = rnum.nextInt(2);
+		int halfday= rnum.nextInt(2);
 		if ( random == 1)
+		{
 			System.out.println("Present" + random);
+			if ( halfday == 0)
+			{
+				System.out.println("Employee present for half day only" + halfday);
+				wage=WagePerHour*HalfDayHour;
+			}
+			else
+			{
+				System.out.println("Employee present for full day" + halfday);
+				wage=WagePerHour*FullDayHour;
+			}
+		}
 		else
+		{
 			System.out.println("Absent" + random);
+			wage=0;
+		}
+		System.out.println("Wage =" +wage);		
 	}
 }
